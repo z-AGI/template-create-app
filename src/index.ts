14d8439ef6d33d4ad901668b01e0e9193c06d635
魏祖潇 <727897965@qq.com>
2025-02-20 17:35:57 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+/**
+ * 箭头函数
+ */
+export const yourFunctionName = async () => {
+
+}
+
+/**
+ * 类
+ */
+export class YourClassName {
+
+}
--- test/index.ts
+++ test/index.ts
@@ -1,26 +1,7 @@
-import { graph } from "../src/graph";
 
-const params = {
-    input: '苹果有什么有害物质？'
-};
-
-const config = {
-    configurable: { thread_id: "test" },
-    streamMode: "updates" as const,
-    recursionLimit: 50
-};
-
-const events = []
-
-const results = (await graph.invoke(params, config)) as any
-
-for (const result of results) {
-    const key = Object.keys(result)?.[0]
-    let event: any = Object.values(result)?.[0]
-    event['__node__'] = key
-    events.push(event)
-}
-
-console.log(events)
+/**
+ * 测试用例
+ */
+console.log("测试结果 =",)
 
 
